26e2b9d30dd4a2238042960a588513e09df78ff5
Ilias HANFAOUI <ilias.hanfaoui@decathlon.com>
2026-02-03 14:59:43 +0100

    feat: exercice 6: normalisation

--- Partie-2/Exercice6.py
+++ Partie-2/Exercice6.py
@@ -1,1 +1,7 @@
-bla bla
+import pandas as pd
+
+df_matchs = pd.read_csv("../data/matchs.csv")
+
+df_matchs["Division"] = df_matchs["Division"].str.strip().str.upper()
+
+print(df_matchs["Division"].unique())
