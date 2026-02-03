485c0bb442ff09f47e90f4a9a4fbb4d54678e8d8
Koman Boni <koman.boni@next-u.fr>
2026-02-03 14:33:06 +0100

    Exercice1

--- Partie-1/Exercice1.py
+++ Partie-1/Exercice1.py
@@ -1,1 +1,6 @@
-bla bla
+#Charge le fichier matches.csv et affiche ses dimensions globales (nombre de lignes et de colonnes)
+
+import pandas as pd
+
+df = pd.read_csv('../data/matchs.csv')
+print(df.shape)
