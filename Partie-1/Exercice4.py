a95cf6d2552300dcb717b3b30b38126d9c89726f
Ilias HANFAOUI <ilias.hanfaoui@decathlon.com>
2026-02-03 14:45:04 +0100

    feat: exercice 4 : selection cible

--- Partie-1/Exercice4.py
+++ Partie-1/Exercice4.py
@@ -3,6 +3,6 @@ import pandas as pd
 df_rating = pd.read_csv("../data/rating.csv")
 df_matchs = pd.read_csv("../data/matchs.csv")
 
-df_equipes_scores = df_matchs[["Date", "Home_Team", "Away_Team", "Home_Score", "Away_Score"]]
+df_equipes_scores = df_matchs[["MatchDate", "HomeTeam", "AwayTeam", "FTHome", "FTAway"]]
 
 print(df_equipes_scores.head())
